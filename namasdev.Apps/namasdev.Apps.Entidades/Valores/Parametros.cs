be60143c289b8a1cadc4f550e9b8ab988b2092fe
Matias Lopez <matutelopez@hotmail.com>
2025-12-22 17:26:23 -0300

    [main] Fixes: parametros, DropDownListMultiple

--- namasdev.Apps/namasdev.Apps.Entidades/Valores/Parametros.cs
+++ namasdev.Apps/namasdev.Apps.Entidades/Valores/Parametros.cs
@@ -7,30 +7,12 @@ namespace namasdev.Apps.Entidades.Valores
 
         public const string SITIO_URL = "SitioUrl";
         public const string ERRORES_MENSAJE_DEFAULT = "ErroresMensajeDefault";
-
-        #endregion
-
-        #region Correos
-
-        public const string CORREOS_REMITENTE = "CorreosRemitente";
-
-        #endregion
-
-        #region Servidor Correos
-
         public const string SERVIDOR_CORREOS = "ServidorCorreos";
-        public const string SERVIDOR_CORREOS_HOST = "ServidorCorreosHost";
-        public const string SERVIDOR_CORREOS_PUERTO = "ServidorCorreosPuerto";
-        public const string SERVIDOR_CORREOS_CREDENCIALES_USUARIO = "ServidorCorreosCredencialesUsuario";
-        public const string SERVIDOR_CORREOS_CREDENCIALES_PASSWORD = "ServidorCorreosCredencialesPassword";
-        public const string SERVIDOR_CORREOS_HABILITAR_SSL = "ServidorCorreosHabilitarSSL";
-        public const string SERVIDOR_CORREOS_COPIA_OCULTA = "ServidorCorreosCopiaOculta";
-        public const string SERVIDOR_CORREOS_HEADERS = "ServidorCorreosHeaders";
 
         #endregion
 
-        #region Azure Storage
-
+        #region Connection Strings
+        
         public const string CLOUD_STORAGE_ACCOUNT_CONNECTION_STRING = "CloudStorageAccountConnectionString";
 
         #endregion
